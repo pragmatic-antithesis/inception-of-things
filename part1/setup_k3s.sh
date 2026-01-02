f31184456f0bf003fb50f5648cafcc79eeca58df
mark <marcos.chiu@gmail.com>
2026-01-02 19:42:45 -0300

    fixed typos

--- part1/setup_k3s.sh
+++ part1/setup_k3s.sh
@@ -5,7 +5,7 @@ ip link set eth1 up
 if [ "$K3S_ROLE" = "server" ]; then
     curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=${NODE_IP} --flannel-iface=eth1" K3S_TOKEN=4242 sh -
     k3s="k3s"
-else if [ "$K3S_ROLE" = "worker"]; then
+elif [ "$K3S_ROLE" = "worker" ]; then
     curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token 4242 --flannel-iface=eth1 --node-ip=${NODE_IP}" sh -s -
     k3s="k3s-agent"
 fi
