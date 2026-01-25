bbf943e00667ca6030a366e06072729d590b18cf
iW90 <ingrid.wagner.dev@gmail.com>
2026-01-25 14:08:05 -0300

    feat: add part2

--- p2/scripts/setup_k3s.sh
+++ p2/scripts/setup_k3s.sh
@@ -1,12 +1,20 @@
 #!/bin/ash
 
-ip addr replace 192.168.56.110/24 dev eth1
+sudo ip addr replace ${NODE_IP}/24 brd 192.168.56.255 dev eth1
 ip link set eth1 up
 ip route replace 192.168.56.0/24 dev eth1
 
-curl -sfL https://get.k3s.io | sh -
+if [ "$K3S_ROLE" = "server" ]; then
+    curl -sfL https://get.k3s.io | \
+      INSTALL_K3S_EXEC="--node-ip=${NODE_IP} --flannel-iface=eth1" sh -
+    k3s="k3s"
+fi
 
-# espera o cluster subir
-sleep 10
+# Wait for Kubernetes API to be ready
+until kubectl get nodes >/dev/null 2>&1; do
+  sleep 2
+done
 
 kubectl apply -f /vagrant/confs
+
+service $k3s restart
